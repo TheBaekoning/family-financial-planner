27db6718d957d46d754cfcb428cc4252b38021f9
Doug Baek <baekdoug@gmail.com>
2020-03-08 13:04:51 -0700

    Initial mw commit

---
+++ family-financial-planner-mw/src/main/java/com/dougbaek/familyfinancialplannermw/FamilyFinancialPlannerMwApplication.java
@@ -0,0 +1,13 @@
+package com.dougbaek.familyfinancialplannermw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyFinancialPlannerMwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyFinancialPlannerMwApplication.class, args);
+	}
+
+}
---
+++ family-financial-planner-mw/src/test/java/com/dougbaek/familyfinancialplannermw/FamilyFinancialPlannerMwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dougbaek.familyfinancialplannermw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyFinancialPlannerMwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
