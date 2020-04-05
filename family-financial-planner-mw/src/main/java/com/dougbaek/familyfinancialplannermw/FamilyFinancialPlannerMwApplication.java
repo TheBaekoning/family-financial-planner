2946e880bb83ad7bbd1d08203bb07b8f79f2b326
thebaekoning <baekdoug@gmail.com>
2020-04-05 15:28:21 -0700

    -created first test controller. we are in business!

--- family-financial-planner-mw/src/main/java/com/dougbaek/familyfinancialplannermw/FamilyFinancialPlannerMwApplication.java
+++ family-financial-planner-mw/src/main/java/com/dougbaek/familyfinancialplannermw/FamilyFinancialPlannerMwApplication.java
@@ -1,9 +1,12 @@
 package com.dougbaek.familyfinancialplannermw;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@AutoConfigurationPackage
 public class FamilyFinancialPlannerMwApplication {
 
 	public static void main(String[] args) {
